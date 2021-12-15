395343a2ef4991ee825cc7400fe3d65b6ba17c71
DeZ <zhilindanilw@gmail.com>
2021-12-15 18:06:13 +0800

    непрерывное размещение

--- MyForm.cpp
+++ MyForm.cpp
@@ -15,404 +15,124 @@ void main()
 
 }
 
-System::Void Project1::MyForm::button3_Click(System::Object^ sender, System::EventArgs^ e)//Данные
+
+System::Void Project1::MyForm::AddButton_Click(System::Object^ sender, System::EventArgs^ e)
 {
-	if (textBox1->Text == ""|| textBox2->Text == "")
+	if (textBox1->Text == "" || textBox3->Text == "")
 	{
-		DefaultParams();
-		textBox1->Text = Convert::ToString(size);
-		textBox2->Text = Convert::ToString(tact);
+		MessageBox::Show("Имя или размер не введены");
 	}
 	else
 	{
-		size = Convert::ToInt16(textBox1->Text);
-		tact= Convert::ToInt16(textBox2->Text);
-	}
+		
+		string value;
+		using namespace Runtime::InteropServices;
+		const char* chars = (const char*)(Marshal::StringToHGlobalAnsi(textBox3->Text)).ToPointer();
+		value = chars;
+		Marshal::FreeHGlobal(IntPtr((void*)chars));
 
-	process* arr = new process[size];
-	for (int i = 0; i < size; i++)
-	{
-		createMas(arr[i]);
-	}
-	int sizeLow = 0, sizeHigh = 0;
-	for (int i = 0; i < size; i++)
-	{
-		if (arr[i].priority == 1)
+		int size = Convert::ToInt16(textBox1->Text);
+		if (size < 256)
 		{
-			sizeHigh++;
+			for (int i = 0; i < 10; i++)
+			{
+				if (arr[i] == "")
+				{
+					arr[i] = value;
+					break;
+				}
+			}
 		}
 		else
 		{
-			sizeLow++;
-		}
-	}
+			double n = size;
+				n/= 256;
+			size =ceil(n);
 
-	process* arrHigh = new process[sizeHigh];
-	process* arrLow = new process[sizeLow];
-
-	for (int i = 0, j = 0, k = 0; i < size; i++)
-	{
-		if (arr[i].priority == 1)
-		{
-			arrHigh[j] = arr[i];
-			j++;
-		}
-		else
-		{
-			arrLow[k] = arr[i];
-			k++;
-		}
-	}
-
-
-	if (sizeHigh == 0)
-	{
-		MessageBox::Show("Очередь Round-robin пуста");
-	}
-	else
-	{
-		int nowWork = 0;
-		int done = 0;
-		string* str = new string[sizeHigh];
-
-		int t = 0;
-
-		while (done < sizeHigh)
-		{
-			if (t % tact == 0 && t != 0)
+			for (int i = 0; i < 10 - size; i++)
 			{
-				nowWork++;
-				if (nowWork >= sizeHigh)
+				bool free = true;
+				for (int j = 0; j < size; j++)
 				{
-					nowWork = 0;
-				}
-				t = 0;
-			}
-			if (arrHigh[nowWork].mas[arrHigh[nowWork].index] != 'И')
-			{
-				nowWork++;
-				if (nowWork >= sizeHigh)
-				{
-					nowWork = 0;
-				}
-				t = 0;
-			}
-
-
-			if (arrHigh[nowWork].mas[arrHigh[nowWork].index] == 'И')
-			{
-				t++;
-				str[nowWork] += ' ';
-				str[nowWork] += arrHigh[nowWork].mas[arrHigh[nowWork].index];
-				arrHigh[nowWork].index++;
-
-				for (int i = 0; i < sizeHigh; i++)
-				{
-					if (i != nowWork)
+					if (arr[i + j] != "")
 					{
-						if (arrHigh[i].mas[arrHigh[i].index] == '\0')
-						{
-							continue;
-						}
-						else if (arrHigh[i].mas[arrHigh[i].index] == 'И')
-						{
-							str[i] += ' ';
-							str[i] += 'Г';
-						}
-						else if (arrHigh[i].mas[arrHigh[i].index] == 'О')
-						{
-							str[i] += ' ';
-							str[i] += 'О';
-							arrHigh[i].index++;
-						}
+						free = false;
 					}
 				}
-			}
-
-			if (sizeHigh - done == 1)
-			{
-				while (arrHigh[nowWork].mas[arrHigh[nowWork].index] != '\0')
+				if (free)
 				{
-					str[nowWork] += ' ';
-					str[nowWork] += arrHigh[nowWork].mas[arrHigh[nowWork].index];
-					arrHigh[nowWork].index++;
+					for (int j = 0; j < size; j++)
+					{
+						arr[i + j] = value;
+					}
+					break;
 				}
 			}
-
-			if (arrHigh[nowWork].mas[arrHigh[nowWork].index] == '\0')
-			{
-				if (arrHigh[nowWork].done == 0)
-				{
-					arrHigh[nowWork].done++;
-					str[nowWork] += ' ';
-					str[nowWork] += 'З';
-					done++;
-					continue;
-				}
-			}
-
 		}
+		textBox1->Text = "";
+		textBox3->Text = "";
+	}
 
-		dataGridView1->RowCount = sizeHigh;
-		dataGridView1->ColumnCount = 1;
+}
 
-		dataGridView1->TopLeftHeaderCell->Value = "Процесс Round-robin";
-		dataGridView1->AutoResizeColumns();
 
-		for (int i = 0; i < dataGridView1->RowCount; i++)
+System::Void Project1::MyForm::DeleteButton_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	if (textBox2->Text == "" )
+	{
+		MessageBox::Show("Имя не введено");
+	}
+	else
+	{
+		string value;
+		using namespace Runtime::InteropServices;
+		const char* chars = (const char*)(Marshal::StringToHGlobalAnsi(textBox2->Text)).ToPointer();
+		value = chars;
+		Marshal::FreeHGlobal(IntPtr((void*)chars));
+
+		for (int i = 0; i < 10; i++)
 		{
-			string mes = str[i];
+			if (arr[i] == value)
+			{
+				arr[i] = "";
+			}
+		}
+		textBox2->Text = "";
+	}
+}
+
+System::Void Project1::MyForm::StartButton_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	dataGridView1->RowCount = 1;
+	dataGridView1->ColumnCount = 10;
+
+	dataGridView1->TopLeftHeaderCell->Value = "Участок памяти";
+	dataGridView1->AutoResizeColumns();
+
+	
+
+	for (int i = 0; i < dataGridView1->RowCount; i++)
+	{
+		
+		dataGridView1->Rows[i]->HeaderCell->Value = "Размер ячейки 256 байт";
+		for (int j = 0; j < dataGridView1->ColumnCount; j++)
+		{
+			string mes = arr[j];
 			String^ mess = gcnew System::String(mes.c_str());
 
-			dataGridView1->Rows[i]->HeaderCell->Value = Convert::ToString(i + 1);
-			dataGridView1->Rows[i]->Cells[0]->Value = mess;
+			dataGridView1->Rows[i]->Cells[j]->Value = mess;
 		}
-
-		dataGridView1->AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode::AutoSizeToAllHeaders);
-		dataGridView1->AutoResizeColumns();
+		
 	}
 
-	if (sizeLow == 0)
-	{
-		MessageBox::Show("Очередь FCFS пуста");
-	}
-	else
-	{
-		int nowWork = 0;
-		int done = 0;
-		bool changeNowWork = false;
-		string* str = new string[sizeLow];
-
-		while (done != sizeLow)
-		{
-			if (arrLow[nowWork].mas[arrLow[nowWork].index] == '\0')
-			{
-				if (arrLow[nowWork].done == 0)
-				{
-					arrLow[nowWork].done++;
-					str[nowWork] += ' ';
-					str[nowWork] += 'З';
-					arrLow[nowWork].willingness = 0;
-					done++;
-					continue;
-				}
-				arrLow[nowWork].willingness = 0;
-				nowWork = findMax(sizeLow, arrLow, nowWork);
-			}
-
-			if (changeNowWork)
-			{
-				arrLow[nowWork].willingness = 0;
-				nowWork = findMax(sizeLow, arrLow, nowWork);
-				changeNowWork = false;
-			}
-
-			if (arrLow[nowWork].mas[arrLow[nowWork].index] != '\0')
-			{
-				while (arrLow[nowWork].mas[arrLow[nowWork].index] != 'О')
-				{
-					str[nowWork] += ' ';
-					str[nowWork] += arrLow[nowWork].mas[arrLow[nowWork].index];
-					arrLow[nowWork].index++;
-
-					for (int i = 0; i < sizeLow; i++)
-					{
-						if (i != nowWork)
-						{
-							if (arrLow[i].mas[arrLow[i].index] == '\0')
-							{
-								arrLow[i].willingness = 0;
-								continue;
-							}
-							else
-							{
-								str[i] += ' ';
-								str[i] += 'Г';
-								arrLow[i].willingness++;
-							}
-						}
-					}
-					if (arrLow[nowWork].mas[arrLow[nowWork].index] == '\0')
-					{
-						if (arrLow[nowWork].done == 0)
-						{
-							arrLow[nowWork].willingness = 0;
-							arrLow[nowWork].done++;
-							str[nowWork] += ' ';
-							str[nowWork] += 'З';
-							done++;
-							break;
-						}
-					}
-				}
-				if (arrLow[nowWork].mas[arrLow[nowWork].index] == 'О')
-				{
-					while (arrLow[nowWork].mas[arrLow[nowWork].index] == 'О')
-					{
-						str[nowWork] += ' ';
-						str[nowWork] += arrLow[nowWork].mas[arrLow[nowWork].index];
-						arrLow[nowWork].index++;
-
-						for (int i = 0; i < sizeLow; i++)
-						{
-							if (i != nowWork)
-							{
-								if (arrLow[i].mas[arrLow[i].index] == '\0')
-								{
-									arrLow[i].willingness = 0;
-									continue;
-								}
-								else
-								{
-									str[i] += ' ';
-									str[i] += 'Г';
-									arrLow[i].willingness++;
-								}
-							}
-						}
-					}
-					changeNowWork = true;
-				}
-			}
-		}
-
-		dataGridView2->RowCount = sizeLow;
-		dataGridView2->ColumnCount = 1;
-
-		dataGridView2->TopLeftHeaderCell->Value = "Процесс FCFS";
-		dataGridView2->AutoResizeColumns();
-
-		for (int i = 0; i < dataGridView2->RowCount; i++)
-		{
-			string mes = str[i];
-			String^ mess = gcnew System::String(mes.c_str());
-
-			dataGridView2->Rows[i]->HeaderCell->Value = Convert::ToString(i + 1);
-			dataGridView2->Rows[i]->Cells[0]->Value = mess;
-		}
-
-		dataGridView2->AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode::AutoSizeToAllHeaders);
-		dataGridView2->AutoResizeColumns();
-	}
-	string tmp = "";
-	for (int i = 0; i < size; i++)
-	{
-		tmp += print(arr[i], size, i + 1);
-	}
-
-	String^ message = gcnew System::String(tmp.c_str());
-	MessageBox::Show(message);
+	dataGridView1->AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode::AutoSizeToAllHeaders);
+	dataGridView1->AutoResizeColumns();
 }
 
-void Project1::MyForm::DefaultParams()
+System::Void Project1::MyForm::MyForm_Load(System::Object^ sender, System::EventArgs^ e)
 {
-	size = 10;
-	tact = 2;
-}
-
-int Project1::MyForm::getPriority()
-{
-	random_device random_device;
-	mt19937 generator(random_device());
-	uniform_int_distribution<> distribution(0, 1);
-
-	return distribution(generator);
-}
-
-void Project1::MyForm::createMas(process& prcss)
-{
-	random_device random_device;
-	mt19937 generator(random_device());
-	uniform_int_distribution<> distribution(1, 10);
-	int size = distribution(generator);
-	char* mas = new char[size];
-
-	uniform_int_distribution<> boolDistribution(0, 1);
-
-	for (int i = 0; i < 1; i++)
+	for (int i = 0; i < 10; i++)
 	{
-		*(mas + i) = 'И';
-
+		arr[i] = "";
 	}
-
-	for (int i = 1; i < size - 1; i++)
-	{
-		int rnd = boolDistribution(generator);
-		if (rnd == 0)
-		{
-			*(mas + i) = 'И';
-		}
-		else
-		{
-			*(mas + i) = 'О';
-		}
-	}
-
-	for (int i = size - 1; i < size; i++)
-	{
-		*(mas + i) = 'И';
-	}
-
-	mas[size] = '\0';
-
-	prcss.mas = mas;
-	prcss.priority = getPriority();
-}
-
-string Project1::MyForm::print(process arr, int size,int index)
-{
-	for (int i = 0, j; i < size; i++)
-	{
-		j = 0;
-
-		string message=to_string(index);
-		message += ": ";
-
-		while (arr.mas[j] != '\0')
-		{
-			message += arr.mas[j];
-			message += " ";
-			j++;
-		}
-
-		message += "\t\t\t";
-
-		if (arr.priority == 1)
-		{
-			message+= "| Высокий приоритет";
-			
-		}
-		else
-		{
-			message += "| Низкий приоритет";
-		}
-		message += "\n";
-		return message;
-	}
-}
-
-int Project1::MyForm::findMax(int size, process arr[], int nowWork)
-{
-	int* maxValue = new int[size];
-
-	for (int i = 0; i < size; i++)
-	{
-		maxValue[i] = 0;
-		if (i != nowWork && arr[i].done == 0)
-		{
-			maxValue[i] = arr[i].willingness;
-		}
-	}
-	int max = 0;
-	int maxIndex = nowWork;
-	for (int i = 0; i < size; i++)
-	{
-		if (maxValue[i] > max)
-		{
-			max = maxValue[i];
-			maxIndex = i;
-		}
-	}
-
-	return maxIndex;
 }
--- MyForm.h
+++ MyForm.h
@@ -1,9 +1,8 @@
 #pragma once
-#include <random>
 #include <string>
+#include <cmath>
 
 namespace Project1 {
-
 	using namespace System;
 	using namespace System::ComponentModel;
 	using namespace System::Collections;
@@ -14,15 +13,6 @@ namespace Project1 {
 	/// <summary>
 	/// Сводка для MyForm
 	/// </summary>
-	
-	public struct process
-	{
-		char* mas;
-		int priority=0;
-		int index = 0;
-		int done = 0;
-		int willingness = 0;
-	};
 
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
@@ -49,17 +39,29 @@ namespace Project1 {
 
 
 
-	private: System::Windows::Forms::Button^ button3;
+
+
+
+
+
+
+
+
 	private: System::Windows::Forms::TextBox^ textBox1;
-
-
+	private: System::Windows::Forms::Button^ AddButton;
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Button^ DeleteButton;
 	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Button^ StartButton;
 	private: System::Windows::Forms::DataGridView^ dataGridView1;
-
-	private: System::Windows::Forms::DataGridView^ dataGridView2;
+	private: System::Windows::Forms::TextBox^ textBox3;
 	private: System::Windows::Forms::Label^ label3;
+
+
+
+
+
 
 
 	protected:
@@ -80,65 +82,80 @@ namespace Project1 {
 			System::Windows::Forms::DataGridViewCellStyle^ dataGridViewCellStyle1 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
 			System::Windows::Forms::DataGridViewCellStyle^ dataGridViewCellStyle2 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
-			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->AddButton = (gcnew System::Windows::Forms::Button());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->DeleteButton = (gcnew System::Windows::Forms::Button());
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->StartButton = (gcnew System::Windows::Forms::Button());
 			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
-			this->dataGridView2 = (gcnew System::Windows::Forms::DataGridView());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->BeginInit();
 			this->SuspendLayout();
-			// 
-			// button3
-			// 
-			this->button3->BackColor = System::Drawing::Color::DarkRed;
-			this->button3->Font = (gcnew System::Drawing::Font(L"Niagara Solid", 9, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->button3->ForeColor = System::Drawing::Color::Black;
-			this->button3->Location = System::Drawing::Point(547, 7);
-			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(527, 46);
-			this->button3->TabIndex = 3;
-			this->button3->Text = L"Запустить";
-			this->button3->UseVisualStyleBackColor = false;
-			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
 			// 
 			// textBox1
 			// 
-			this->textBox1->BackColor = System::Drawing::Color::DarkRed;
-			this->textBox1->Location = System::Drawing::Point(14, 25);
+			this->textBox1->Location = System::Drawing::Point(14, 135);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(258, 20);
-			this->textBox1->TabIndex = 4;
+			this->textBox1->Size = System::Drawing::Size(93, 20);
+			this->textBox1->TabIndex = 12;
+			// 
+			// AddButton
+			// 
+			this->AddButton->Location = System::Drawing::Point(14, 161);
+			this->AddButton->Name = L"AddButton";
+			this->AddButton->Size = System::Drawing::Size(93, 23);
+			this->AddButton->TabIndex = 13;
+			this->AddButton->Text = L"Добавить файл";
+			this->AddButton->UseVisualStyleBackColor = true;
+			this->AddButton->Click += gcnew System::EventHandler(this, &MyForm::AddButton_Click);
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(62, 7);
+			this->label1->Location = System::Drawing::Point(14, 117);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(140, 15);
-			this->label1->TabIndex = 7;
-			this->label1->Text = L"Количество процессов";
+			this->label1->Size = System::Drawing::Size(93, 15);
+			this->label1->TabIndex = 14;
+			this->label1->Text = L"Размер файла";
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(390, 3);
+			this->label2->Location = System::Drawing::Point(18, 240);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(34, 15);
-			this->label2->TabIndex = 10;
-			this->label2->Text = L"Такт";
+			this->label2->Size = System::Drawing::Size(74, 15);
+			this->label2->TabIndex = 17;
+			this->label2->Text = L"Имя файла";
+			// 
+			// DeleteButton
+			// 
+			this->DeleteButton->Location = System::Drawing::Point(12, 284);
+			this->DeleteButton->Name = L"DeleteButton";
+			this->DeleteButton->Size = System::Drawing::Size(95, 23);
+			this->DeleteButton->TabIndex = 16;
+			this->DeleteButton->Text = L"Удалить";
+			this->DeleteButton->UseVisualStyleBackColor = true;
+			this->DeleteButton->Click += gcnew System::EventHandler(this, &MyForm::DeleteButton_Click);
 			// 
 			// textBox2
 			// 
-			this->textBox2->BackColor = System::Drawing::Color::DarkRed;
-			this->textBox2->Location = System::Drawing::Point(282, 25);
+			this->textBox2->Location = System::Drawing::Point(12, 258);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(258, 20);
-			this->textBox2->TabIndex = 9;
+			this->textBox2->Size = System::Drawing::Size(93, 20);
+			this->textBox2->TabIndex = 15;
+			// 
+			// StartButton
+			// 
+			this->StartButton->Location = System::Drawing::Point(14, 12);
+			this->StartButton->Name = L"StartButton";
+			this->StartButton->Size = System::Drawing::Size(93, 23);
+			this->StartButton->TabIndex = 18;
+			this->StartButton->Text = L"Старт";
+			this->StartButton->UseVisualStyleBackColor = true;
+			this->StartButton->Click += gcnew System::EventHandler(this, &MyForm::StartButton_Click);
 			// 
 			// dataGridView1
 			// 
@@ -157,52 +174,49 @@ namespace Project1 {
 			dataGridViewCellStyle2->BackColor = System::Drawing::Color::White;
 			dataGridViewCellStyle2->Font = (gcnew System::Drawing::Font(L"Modern No. 20", 8.999999F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			dataGridViewCellStyle2->ForeColor = System::Drawing::Color::Black;
+			dataGridViewCellStyle2->ForeColor = System::Drawing::Color::DarkRed;
 			dataGridViewCellStyle2->SelectionBackColor = System::Drawing::SystemColors::Highlight;
 			dataGridViewCellStyle2->SelectionForeColor = System::Drawing::Color::White;
 			dataGridViewCellStyle2->WrapMode = System::Windows::Forms::DataGridViewTriState::False;
 			this->dataGridView1->DefaultCellStyle = dataGridViewCellStyle2;
 			this->dataGridView1->GridColor = System::Drawing::Color::White;
-			this->dataGridView1->Location = System::Drawing::Point(14, 75);
+			this->dataGridView1->Location = System::Drawing::Point(113, 12);
 			this->dataGridView1->Name = L"dataGridView1";
-			this->dataGridView1->Size = System::Drawing::Size(519, 331);
+			this->dataGridView1->Size = System::Drawing::Size(528, 312);
 			this->dataGridView1->TabIndex = 11;
 			// 
-			// dataGridView2
+			// textBox3
 			// 
-			this->dataGridView2->BackgroundColor = System::Drawing::Color::DarkRed;
-			this->dataGridView2->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView2->Location = System::Drawing::Point(547, 75);
-			this->dataGridView2->Name = L"dataGridView2";
-			this->dataGridView2->Size = System::Drawing::Size(527, 331);
-			this->dataGridView2->TabIndex = 13;
+			this->textBox3->Location = System::Drawing::Point(14, 94);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(93, 20);
+			this->textBox3->TabIndex = 19;
 			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->BackColor = System::Drawing::SystemColors::ActiveCaptionText;
-			this->label3->Font = (gcnew System::Drawing::Font(L"Modern No. 20", 12, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label3->Location = System::Drawing::Point(37, 423);
+			this->label3->Location = System::Drawing::Point(18, 76);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(465, 18);
-			this->label3->TabIndex = 14;
-			this->label3->Text = L"И - исполняется, О - ожидает, Г - готов к иполнению, З - завершён\r\n";
+			this->label3->Size = System::Drawing::Size(74, 15);
+			this->label3->TabIndex = 20;
+			this->label3->Text = L"Имя файла";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(7, 15);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::WindowText;
-			this->ClientSize = System::Drawing::Size(1088, 459);
+			this->ClientSize = System::Drawing::Size(653, 336);
 			this->Controls->Add(this->label3);
-			this->Controls->Add(this->dataGridView2);
-			this->Controls->Add(this->dataGridView1);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->StartButton);
 			this->Controls->Add(this->label2);
+			this->Controls->Add(this->DeleteButton);
 			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->label1);
+			this->Controls->Add(this->AddButton);
 			this->Controls->Add(this->textBox1);
-			this->Controls->Add(this->button3);
+			this->Controls->Add(this->dataGridView1);
 			this->Cursor = System::Windows::Forms::Cursors::PanNW;
 			this->Font = (gcnew System::Drawing::Font(L"Modern No. 20", 8.999999F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
@@ -211,27 +225,20 @@ namespace Project1 {
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->Name = L"MyForm";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
-			this->Text = L"Многоуровневая очередь";
+			this->Text = L"Непрерывное размещение файлов";
 			this->TransparencyKey = System::Drawing::Color::Lime;
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e);
-	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
-	} 
-	
-	private: void DefaultParams();
-	private: int size,tact;
-
-	public: int getPriority();
-	public:	void createMas(process& prcss);
-	public: string print(process arr, int size,int index);
-	public: int findMax(int size, process arr[], int nowWork);
+private: System::Void AddButton_Click(System::Object^ sender, System::EventArgs^ e);
+private: System::Void DeleteButton_Click(System::Object^ sender, System::EventArgs^ e);
+private: System::Void StartButton_Click(System::Object^ sender, System::EventArgs^ e);
+public: static string *arr = new string[10];
+private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e);
 };
 
 
